07567fb2c91226dd211269ca3a6d62f82cf09447
QingDengKuZhu <2583746755@qq.com>
2014-02-19 16:34:54 +0800

    顺序存储的栈的数据结构定义以及支持的基本操作

---
+++ SqStack/Data.h
@@ -0,0 +1,23 @@
+﻿/*****************
+**栈的顺序数据结构
+*****************/
+#ifndef DATA_H
+#define DATA_H
+
+#include "Main_First.h"
+
+#define STACK_INIT_SIZE 10		/*栈的初始分配量.*/
+#define STACK_INCREMENT 5		/*栈的每次分配增量.*/
+
+typedef int Elem;
+
+typedef struct SqStack
+{
+	Elem *Base;			/*栈底指针.*/
+	Elem *Top;			/*栈顶指针,始终指向栈顶元素的下一个位置.*/
+	size_t stacksize;	/*当前栈允许的的最大长度(即当前栈空间分配量).*/
+
+}SQSTACK, *PSQSTACK;
+
+
+#endif
---
+++ SqStack/Data_Base.h
@@ -0,0 +1,40 @@
+﻿/**********************
+**栈的基本操作.
+***********************/
+#ifndef DATA_BASE_H
+#define DATA_BASE_H
+
+#include "Main_First.h"
+#include "Data.h"
+/*
+pS为指向栈的指针,且pS不为NULL.
+*/
+
+/*构造一个空的栈.*/
+void InitStack(PSQSTACK pS);
+
+/*销毁栈.*/
+void DestroyStack(PSQSTACK pS);
+
+/*将栈清空.*/
+void ClearStack(PSQSTACK pS);
+
+/*判断栈是否为空.若为空,函数返回TRUE;否者函数返回FALSE.*/
+BOOL StackEmpty(PSQSTACK pS);
+
+/*返回栈中元素个数,即长度.*/
+size_t StackLength(PSQSTACK pS);
+
+/*获取栈顶元素.若操作成功,函数返回OK,*e保存栈顶的值;若操作失败,函数返回FAILE,*e为垃圾值.*/
+STATUS GetTop(PSQSTACK pS, Elem *e);
+
+/*插入新的数据元素v为栈顶元素.若成功,函数返回OK;否者返回FAILE.*/
+STATUS Push(PSQSTACK pS, const Elem v);
+
+/*删除栈顶元素.若成功删除,函数返回OK,*e保存被删除元素的值;否者函数返回FAILE,*e为垃圾值.*/
+STATUS DeleteStack(PSQSTACK pS, const size_t pos, Elem *e);
+
+/*从栈底到栈顶依次输出栈中元素.*/
+void TraveStack(PSQSTACK pS);
+
+#endif
