41ae6de31d179cc13389c0fcf2419a270a3dd9da
QingDengKuZhu <2583746755@qq.com>
2014-02-19 20:38:40 +0800

    测试用例

---
+++ SqStack/Main.c
@@ -0,0 +1,135 @@
+﻿/**********************
+**主函数, 测试
+***********************/
+
+#include "Main_First.h"
+#include "Data.h"
+#include "Data_Base.h"
+#include "Test.h"
+#include <time.h> /*随机数*/
+
+int main(void)
+{
+	int select;		/*保存选择变量*/
+	Elem e;			/*保存从函数返回的结点的值*/
+	Elem v;			/*保存传递给函数的结点的值*/
+	
+	size_t i= 0;
+	SQSTACK S;
+	InitStack(&S);	/*初始化栈*/
+
+	srand((int)time(NULL));
+
+	while (i < 20)
+	{
+
+		Push(&S, rand()%20);
+		++i;
+	}
+
+	while (1)	/*while_@1*/
+	{
+		if (!(S.Base))
+		{
+			printf("栈不存在!\n");
+			break;
+		}
+
+		system("cls");
+		Menu();
+
+		printf("请输入您的选择(1~10):");
+		scanf("%d", &select);
+		getchar();
+
+		switch (select)	/*switch_@1*/
+		{
+		case 1:			/*入栈.*/
+			v = InputValue("入栈元素为:");
+			if (FAILE == Push(&S, v))
+			{
+				printf("入栈失败!\n");
+			}
+			else
+			{
+				printf("入栈成功!\n");
+			}
+			
+			getchar();
+			break;
+		case 2:			/*输出栈中元素.*/
+			printf("栈为:");
+			TraveStack(&S);
+			
+			getchar();
+			break;
+		case 3:			/*出栈.*/
+			if (OK == Pop(&S, &e))
+			{
+				printf("删除成功,出栈元素是%d!\n", e);
+			}
+			else
+			{
+				printf("删除失败!\n");
+			}
+
+			getchar();
+			break;
+
+		case 4:			/*输出栈的长度.*/
+			printf("栈长为: %d \n", StackLength(&S));
+			
+			getchar();
+			break;
+
+		case 5:			/*清空栈.*/
+			ClearStack(&S);
+			printf("该栈已经清空!\n");
+			
+			getchar();
+			break;
+	
+		case 6:			/*销毁栈.*/
+			DestroyStack(&S);
+			printf("栈已删除!\n");
+			
+			getchar();	
+			break;
+			
+		case 7:			/*返回栈顶结点元素.*/
+			if (OK == GetTop(&S, &e))
+			{
+				printf("栈顶为:%d\n", e);
+			}
+			else
+			{
+				printf("不存在栈顶元素!\n");
+			}
+
+			getchar();	
+			break;
+
+		case 8:			/*判断栈是否为空.*/
+			if (StackEmpty(&S) == TRUE)
+			{
+				printf("栈为空!\n");
+			}
+			else
+			{
+				printf("栈非空!\n");
+			}
+
+			getchar();	
+			break;
+		
+		default:
+			printf("请重新选择!\n");
+			
+			getchar();
+			break;
+		}/*switch_@1*/
+
+	}	/*while_@1*/
+	
+	return EXIT_SUCCESS;
+}
---
+++ SqStack/Test.c
@@ -0,0 +1,83 @@
+﻿#include "Test.h"
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+
+void gotoxy(const short x, const short y)
+{
+	COORD pos;	/*COORD在windows.h中定义*/
+	pos.X = x;
+	pos.Y = y;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); /* 设置光标位置 */
+
+	return;
+}
+
+
+void Menu(void)
+{
+	system("cls");
+	gotoxy(10, 5);                        /*在文本窗口中设置光标*/
+	cprintf("			顺序栈的测试			");
+	gotoxy(8, 8);
+	cprintf("-----------------------------Menu-----------------------------\n");
+	gotoxy(8, 9);
+	cprintf("| 1.入栈                     2.输出栈                  |\n");
+	gotoxy(8, 10);
+	cprintf("| 3.出栈                     4.输出栈长度                  |\n");
+	gotoxy(8, 11);
+	cprintf("| 5.清空栈                     6.销毁栈  |\n");
+	gotoxy(8, 12);
+	cprintf("| 7.返回栈顶元素             8.判断栈是否为空            |\n");
+	gotoxy(8, 13);
+	cprintf("| 9.删除整表                     10.求中间结点               |\n");	
+	gotoxy(8, 16);
+	cprintf("--------------------------------------------------------------\n");
+	
+    return;
+}
+
+size_t InputPos(const char notice[])
+{
+	int pos;		/*保存待输入的位序*/
+	unsigned n;		/*保存scanf的返回值*/
+	
+	do 
+	{
+		printf(notice);
+		n = scanf("%d", &pos);
+		fflush(stdin);
+
+		/*判断输入的位序是否在合理的范围内*/
+		if (pos<1 || n!=1)
+		{
+			printf("位序必须大于0!!!\n");
+		}
+	} while (pos<1 || n!=1);
+
+	return (size_t)pos;
+}
+
+int InputValue(const char notice[])
+{	
+	int e;
+	unsigned n;
+
+	do 
+	{
+		printf(notice);
+		n = scanf("%d", &e);
+		fflush(stdin);
+
+		if (n != 1)
+		{
+			printf("请输入满足条件的数值!\n");
+		}
+
+	} while (n!=1);
+	
+	return e;
+}
