116d67102008a97113365fe710b63097c0f16e0c
QingDengKuZhu <2583746755@qq.com>
2014-02-20 15:11:07 +0800

    添加扩展操作

---
+++ SqStack/Data_Expand.c
@@ -0,0 +1,28 @@
+﻿#include "Data_Base.h"
+
+void Conversion_ten_to_n(unsigned int m, unsigned int n)
+{
+	char ch[16] = {'0', '1', '2', '3', 
+				   '4', '5', '6', '7', 
+				   '8', '9', 'A', 'B', 
+				   'C', 'D', 'E', 'F'};
+	SQSTACK S;
+	InitStack(&S);
+
+	while (m != 0)
+	{
+		Push(&S, m%n);
+		m/=n;
+	}
+
+	printf("十进制数%d转换为%d进制数为: ", m, n);
+	
+	while (FALSE == StackEmpty(&S))
+	{
+		unsigned int temp;
+		Pop(&S, &temp);
+		printf("%c", ch[temp]);
+	}
+
+	return;
+}
---
+++ SqStack/Data_Expand.h
@@ -0,0 +1,14 @@
+﻿/*************************************
+**顺序栈的扩展操作
+**************************************/
+#ifndef DATA_EXPAND_H
+#define DATA_EXPAND_H
+
+#include "Data.h"
+#include "Main_First.h"
+#include "Data_Base.h"
+
+/*将任意十进制数m转换成n进制数(2<=n<=16)输出.*/
+void Conversion_ten_to_n(unsigned int m, unsigned int n);
+
+#endif
