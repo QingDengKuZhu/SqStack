ccb5a17bac786504f72eb0e6aa21fe9b2edfb45e
QingDengKuZhu <2583746755@qq.com>
2014-02-20 20:13:09 +0800

    添加扩展操作
    逆波兰计算器

--- SqStack/Data_Expand.c
+++ SqStack/Data_Expand.c
@@ -1,4 +1,5 @@
 ﻿#include "Data_Base.h"
+#include "ctype.h"
 
 void Conversion_ten_to_n(unsigned int m, unsigned int n)
 {
@@ -25,6 +26,107 @@ void Conversion_ten_to_n(unsigned int m, unsigned int n)
 	}
 	
 	DestroyStack(&S);
+	
+	return;
+}
+
+void ReversePolishNotation(void)
+{
+	SQSTACK S;
+	char c;	
+	int result;		/*结果*/
+	char str[10];	/*缓冲区*/
+	size_t i = 0;
+	int a, b;	/*操作数*/
+
+	InitStack(&S);
+
+	printf("请按照逆波兰表达式输入带计算数据,数据与运算符之间以空格间隔,以#结尾!\n");
+	scanf("%c", &c);	
+
+	while (c != '#')
+	{
+		while (0 != isdigit(c))	/*过滤数字*/
+		{
+			str[i++] = c;
+			str[i] = '\0';
+			if (i == 10)
+			{
+				printf("出错,输入的单个数据过大!\n");
+			
+				exit(ERROR);
+			}
+
+			scanf("%c", &c);
+			if (' ' == c)
+			{
+				Push(&S, atoi(str));	/*将字符转换成数字压入栈中.*/
+			}
+		}
+
+		i = 0;
+
+		switch (c)
+		{
+		case '+':
+			Pop(&S, &b);
+			Pop(&S, &a);
+			Push(&S, a+b);
+	
+			break;	
+			
+		case '-':
+			Pop(&S, &b);
+			Pop(&S, &a);
+			Push(&S, a-b);	/*注意相减的顺序*/				
+	
+			break;
+		
+		case '*':
+			Pop(&S, &b);	
+			Pop(&S, &a);
+			Push(&S, a*b);
+
+			break;
+
+		case '/':	/*注意谁是除数谁是被除数,以及除数不能为0!*/
+			Pop(&S, &b);
+			Pop(&S, &a);
+		
+			if (b == 0)
+			{
+				printf("除数不能为0!\n");
+				exit(ERROR);
+			}
+
+			Push(&S, a/b);
+				
+			break;
+	
+		case ' ':
+			
+			break;
+
+		default:
+			printf("不能用两个空格间隔!\n");
+			exit(ERROR);
+			break;
+		}
+
+		scanf("%c", &c);
+	}
+	
+	Pop(&S, &result);
+	/*若输入的是逆波兰表达式,则最后栈一定为空!*/
+	if (TRUE != StackEmpty(&S))
+	{
+		printf("输入的不是逆波兰表达式!!!\n");
+		exit(ERROR);
+	}
+				
+	printf("逆波兰表达式的结果是: %d", result);
+	
+	getchar();
 
 	return;
 }
